b0543bcad02e92258584c9c5105db9b3f4c3a3e3
Konstantin Davydov <davydovks@mail.ru>
2023-07-31 18:51:45 +0500

    clean index.php

--- public/index.php
+++ public/index.php
@@ -1,6 +1,5 @@
 <?php
 
-// Подключение автозагрузки через composer
 require __DIR__ . '/../vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
@@ -9,9 +8,7 @@ $app = AppFactory::create();
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    $response->getBody()->write('Welcome to Slim!!!');
-    return $response;
-    // Благодаря пакету slim/http этот же код можно записать короче
-    // return $response->write('Welcome to Slim!');
+    return $response->write('Welcome to Slim!');
 });
+
 $app->run();
