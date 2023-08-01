61dda29a4443799d042c2de7aa5bfb2a419d2a41
Konstantin Davydov <davydovks@mail.ru>
2023-08-01 18:47:25 +0500

    add using container and Twig

--- public/index.php
+++ public/index.php
@@ -2,15 +2,26 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use DI\Container;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+use Slim\Middleware\MethodOverrideMiddleware;
 
-$app = AppFactory::create();
+$container = new Container();
 
+$container->set('view', function () {
+    return Twig::create(__DIR__ . '/../templates');
+});
+
+$app = AppFactory::createFromContainer($container);
+
+$app->add(TwigMiddleware::createFromContainer($app));
 $app->add(MethodOverrideMiddleware::class);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
-    return $response->write('Welcome to Slim!');
+    return $response->write('Welcome to Slim!!');
 });
 
 $app->run();
