909e4598f6e75e0f688465ab4d4fd6e060f718e4
Konstantin Davydov <davydovks@mail.ru>
2023-08-01 17:56:42 +0500

    add MethodOverrideMiddleware

--- public/index.php
+++ public/index.php
@@ -5,6 +5,8 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
+
+$app->add(MethodOverrideMiddleware::class);
 $app->addErrorMiddleware(true, true, true);
 
 $app->get('/', function ($request, $response) {
