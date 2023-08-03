c29cd7ff8808e50b37e75076bb6f065d7fedbe34
Konstantin Davydov <davydovks@mail.ru>
2023-08-03 15:51:06 +0500

    add urls.store and urls.show routes

--- public/index.php
+++ public/index.php
@@ -3,10 +3,15 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use DI\Container;
+use Repository\CookieRepository;
 use Slim\Factory\AppFactory;
 use Slim\Views\Twig;
 use Slim\Views\TwigMiddleware;
 use Slim\Middleware\MethodOverrideMiddleware;
+
+session_start();
+
+$repo = new CookieRepository('urls');
 
 $container = new Container();
 
@@ -24,8 +29,39 @@ $app->get('/', function ($request, $response) {
     return $this->get('view')->render($response, 'index.twig');
 })->setName('home');
 
-$app->get('/urls', function ($request, $response) {
-    return $this->get('view')->render($response, 'urls/index.twig');
+$app->get('/urls', function ($request, $response) use ($repo) {
+    $urls = $repo->all($request);
+
+    return $this->get('view')->render($response, 'urls/index.twig', [
+        'urls' => $urls
+    ]);
 })->setName('urls.index');
+
+$app->post('/urls', function ($request, $response) use ($repo) {
+    $url = $request->getParsedBodyParam('url');
+
+    //$validator = new Validators\UserValidator();
+    $errors = []; //$validator->validate($user);
+
+    if (count($errors) === 0) {
+        $repo->save($url, $request, $response);
+        //$this->get('flash')->addMessage('success', 'User added successfully!');
+
+        return $response->withRedirect('/urls', 302);
+        //return $response->withRedirect($router->urlFor('users.index'), 302);
+    }
+
+    return $this->get('view')->render($response, 'index.twig', [
+        'url' => $url,
+        'errors' => $errors
+    ]);
+})->setName('urls.store');
+
+$app->get('/urls/{id}', function ($request, $response, $args) {
+    return $this->get('view')->render($response, 'urls/show.twig', [
+        'id' => $args['id']
+    ]);
+})->setName('urls.show');
+
 
 $app->run();
