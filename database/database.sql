567d731541a1ca68eb82c99f51069a2601ecbaa6
Konstantin Davydov <davydovks@mail.ru>
2023-08-07 19:24:22 +0500

    change sql file to CREATE TABLE IF NOT EXISTS

--- database/database.sql
+++ database/database.sql
@@ -1,12 +1,12 @@
 -- CREATE DATABASE IF NOT EXISTS page_analyzer;
-DROP TABLE IF EXISTS urls;
-CREATE TABLE urls (
+-- DROP TABLE IF EXISTS urls;
+CREATE TABLE IF NOT EXISTS urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255),
     created_at timestamp
 );
-DROP TABLE IF EXISTS checks;
-CREATE TABLE checks (
+-- DROP TABLE IF EXISTS checks;
+CREATE TABLE IF NOT EXISTS checks (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     status int,
     h1 varchar(255),
