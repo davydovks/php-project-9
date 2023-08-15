c5257a55fa033728cfb6ac87f28e35b0bcdd9629
Konstantin Davydov <davydovks@mail.ru>
2023-08-15 21:05:06 +0500

    add html parsing to checks.store route

--- public/index.php
+++ public/index.php
@@ -117,9 +117,18 @@ $app->post('/urls/{url_id}/checks', function ($request, $response, $args) use ($
             ->withStatus(500);
     }
 
-    // Добавить отправку и обработку HTTP-запроса
+    $client = new \GuzzleHttp\Client();
+    $urlResponse = $client->request('GET', $url['name']);
+    $body = $urlResponse->getBody();
+    preg_match('/(?<=>)(.*?)(?=<\/h1>)/', $body, $h1Matches);
+    preg_match('/(?<=title>)(.*?)(?=<\/title>)/', $body, $titleMatches);
+    preg_match('/(?<=\<meta name=\"description\" content=\")(.*?)(?=\">)/', $body, $descMatches);
     $check = [
         'url_id' => $url['id'],
+        'status_code' => $urlResponse->getStatusCode(),
+        'h1' => $h1Matches[0],
+        'title' => $titleMatches[0],
+        'description' => $descMatches[0],
         'created_at' => Carbon::now()->toDateTimeString()
     ];
     $repoChecks->save($check);
