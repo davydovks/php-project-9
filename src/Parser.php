f68f259b59d9b6fdfce58e123fd35d574de6921c
Konstantin Davydov <davydovks@mail.ru>
2023-08-17 16:58:31 +0500

    catch http request errors

--- src/Parser.php
+++ src/Parser.php
@@ -3,25 +3,30 @@
 namespace PageAnalyzer;
 
 use Carbon\Carbon;
+use Exception;
 
 class Parser
 {
     public static function getUrlData(array $url): array
     {
         $client = new \GuzzleHttp\Client();
-        $urlResponse = $client->request('GET', $url['name']);
-        $body = $urlResponse->getBody();
-        preg_match('/(?<=>)(.*?)(?=<\/h1>)/', $body, $h1Matches);
-        preg_match('/(?<=title>)(.*?)(?=<\/title>)/', $body, $titleMatches);
-        preg_match('/(?<=\<meta name=\"description\" content=\")(.*?)(?=\">)/', $body, $descMatches);
-        $check = [
-            'url_id' => $url['id'],
-            'status_code' => $urlResponse->getStatusCode(),
-            'h1' => $h1Matches[0],
-            'title' => $titleMatches[0],
-            'description' => $descMatches[0],
-            'created_at' => Carbon::now()->toDateTimeString()
-        ];
+        try {
+            $urlResponse = $client->request('GET', $url['name']);
+            $body = $urlResponse->getBody();
+            preg_match('/(?<=>)(.*?)(?=<\/h1>)/', $body, $h1Matches);
+            preg_match('/(?<=title>)(.*?)(?=<\/title>)/', $body, $titleMatches);
+            preg_match('/(?<=\<meta name=\"description\" content=\")(.*?)(?=\">)/', $body, $descMatches);
+            $check = [
+                'url_id' => $url['id'],
+                'status_code' => $urlResponse->getStatusCode(),
+                'h1' => $h1Matches[0],
+                'title' => $titleMatches[0],
+                'description' => $descMatches[0],
+                'created_at' => Carbon::now()->toDateTimeString()
+            ];
+        } catch (Exception $e) {
+            $check = [];
+        } 
 
         return $check;
     }
