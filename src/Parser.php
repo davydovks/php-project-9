21199ce2e81f43ef08a1d010fda02ce7cfde92d7
Konstantin Davydov <davydovks@mail.ru>
2023-08-24 10:25:20 +0500

    use helper optional() for parsing

--- src/Parser.php
+++ src/Parser.php
@@ -15,13 +15,13 @@ class Parser
             $urlResponse = $client->get($url['name']);
 
             $document = new Document($url['name'], true);
-            $h1 = $document->find('h1')[0]->innerHtml() ?? '';
-            $title = $document->find('title')[0]->innerHtml() ?? '';
-            $description = $document->find('meta[name=description]')[0]->content ?? '';
+            $h1 = optional($document->find('h1')[0])->innerHtml() ?? '';
+            $title = optional($document->find('title')[0])->innerHtml() ?? '';
+            $description = optional($document->find('meta[name=description]')[0])->content ?? '';
 
             $check = [
                 'url_id' => $url['id'],
-                'status_code' => $urlResponse->getStatusCode(),
+                'status_code' => optional($urlResponse)->getStatusCode(),
                 'h1' => mb_substr($h1, 0, 255),
                 'title' => mb_substr($title, 0, 255),
                 'description' => mb_substr($description, 0, 255),
