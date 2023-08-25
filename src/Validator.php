8e2b036056ba9a69f4a36bb4b84b7717a99b8899
Konstantin Davydov <davydovks@mail.ru>
2023-08-25 10:14:05 +0500

    add Validator module

---
+++ src/Validator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Validator;
+
+use Valitron\Validator;
+
+function createNameValidator(array $normalizedUrl): Validator
+{
+    $validator = new Validator($normalizedUrl);
+    $validator->rule('required', 'name');
+    $validator->rule('url', 'name');
+    $validator->rule('lengthMax', 'name', 255);
+
+    return $validator;
+}
+
+function translateNameValidationErrors($errors)
+{
+    if (!isset($errors['name'])) {
+        return [];
+    }
+
+    if (in_array('Name is required', $errors['name'])) {
+        $message = "URL не должен быть пустым";
+    } elseif (in_array('Name is not a valid URL', $errors['name'])) {
+        $message = "Некорректный URL";
+    } elseif (count($errors['name']) > 0) {
+        $message = $errors['name'][0];
+    } else {
+        $message = 'nothing';
+    }
+    return ['name' => $message];
+}
