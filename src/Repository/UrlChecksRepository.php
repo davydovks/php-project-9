fff3daa5eb440ae80458bc8e436b7f3390c84082
Konstantin Davydov <davydovks@mail.ru>
2023-09-18 14:47:06 +0500

    replace bindParam with bindValue

--- src/Repository/UrlChecksRepository.php
+++ src/Repository/UrlChecksRepository.php
@@ -23,7 +23,7 @@ class UrlChecksRepository
         $sql = "SELECT * FROM url_checks WHERE url_id = ? ORDER BY id DESC";
         try {
             $stmt = $this->pdo->prepare($sql);
-            $stmt->bindParam(1, $urlId, \PDO::PARAM_INT);
+            $stmt->bindValue(1, $urlId, \PDO::PARAM_INT);
             $stmt->execute();
             $checks = $stmt->fetchAll(\PDO::FETCH_ASSOC);
             return array_map(function ($check) {
@@ -40,7 +40,7 @@ class UrlChecksRepository
         $sql = "SELECT * FROM url_checks WHERE url_id = ? ORDER BY id DESC LIMIT 1";
         try {
             $stmt = $this->pdo->prepare($sql);
-            $stmt->bindParam(1, $urlId, \PDO::PARAM_INT);
+            $stmt->bindValue(1, $urlId, \PDO::PARAM_INT);
             $stmt->execute();
             $check = $stmt->fetch(\PDO::FETCH_ASSOC);
             return is_array($check) ? new UrlCheck($check) : null;
@@ -57,12 +57,12 @@ class UrlChecksRepository
         $sql = "INSERT INTO url_checks ({$columns}) VALUES ({$values}) RETURNING id";
         try {
             $stmt = $this->pdo->prepare($sql);
-            $stmt->bindParam(1, $urlCheck->getUrlId());
-            $stmt->bindParam(2, $urlCheck->getStatusCode());
-            $stmt->bindParam(3, $urlCheck->getH1());
-            $stmt->bindParam(4, $urlCheck->getTitle());
-            $stmt->bindParam(5, $urlCheck->getDescription());
-            $stmt->bindParam(6, $urlCheck->getCreatedAt());
+            $stmt->bindValue(1, $urlCheck->getUrlId());
+            $stmt->bindValue(2, $urlCheck->getStatusCode());
+            $stmt->bindValue(3, $urlCheck->getH1());
+            $stmt->bindValue(4, $urlCheck->getTitle());
+            $stmt->bindValue(5, $urlCheck->getDescription());
+            $stmt->bindValue(6, $urlCheck->getCreatedAt());
             $stmt->execute();
             [$id] = $stmt->fetch(\PDO::FETCH_NUM);
             return $id;
--- src/Repository/UrlsRepository.php
+++ src/Repository/UrlsRepository.php
@@ -53,7 +53,7 @@ class UrlsRepository
         $sql = "SELECT * FROM urls WHERE name = ?";
         try {
             $stmt = $this->pdo->prepare($sql);
-            $stmt->bindParam(1, $name);
+            $stmt->bindValue(1, $name);
             $stmt->execute();
             $url = $stmt->fetch(\PDO::FETCH_ASSOC);
             return is_array($url) ? new Url($url) : null;
@@ -68,8 +68,8 @@ class UrlsRepository
         $sql = "INSERT INTO urls (name, created_at) VALUES (?, ?) RETURNING id";
         try {
             $stmt = $this->pdo->prepare($sql);
-            $stmt->bindParam(1, $url->getName());
-            $stmt->bindParam(2, $url->getCreatedAt());
+            $stmt->bindValue(1, $url->getName());
+            $stmt->bindValue(2, $url->getCreatedAt());
             $stmt->execute();
             [$id] = $stmt->fetch(\PDO::FETCH_NUM);
             return $id;
