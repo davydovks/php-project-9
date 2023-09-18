00a14f2e6dd0073a1c5c75131ea7d61f36de4e38
Konstantin Davydov <davydovks@mail.ru>
2023-09-18 14:17:16 +0500

    update parser to work with entity classes

--- src/Parser.php
+++ src/Parser.php
@@ -2,13 +2,14 @@
 
 namespace PageAnalyzer;
 
-use Carbon\Carbon;
+use App\Entity\Url;
+use App\Entity\UrlCheck;
 use DiDom\Document;
 use Psr\Http\Message\ResponseInterface;
 
 class Parser
 {
-    public static function parseResponse(ResponseInterface $urlResponse): array
+    public static function parseResponse(ResponseInterface $urlResponse): UrlCheck
     {
         $document = new Document($urlResponse->getBody()->__toString());
         $h1 = optional($document->first('h1'))->innerHtml() ?? '';
@@ -19,20 +20,19 @@ class Parser
             'status_code' => $urlResponse->getStatusCode(),
             'h1' => mb_substr($h1, 0, 255),
             'title' => mb_substr($title, 0, 255),
-            'description' => mb_substr($description, 0, 255),
-            'created_at' => Carbon::now()->toDateTimeString()
+            'description' => mb_substr($description, 0, 255)
         ];
 
-        return $check;
+        return new UrlCheck($check);
     }
 
-    public static function normalizeUrl(array $rawUrl): array
+    public static function normalizeUrl(array $rawUrl): Url
     {
         $parsedUrl = parse_url($rawUrl['name']);
         $scheme = isset($parsedUrl['scheme']) ? $parsedUrl['scheme'] . '://' : '';
         $host = isset($parsedUrl['host']) ? $parsedUrl['host'] : '';
         $normalizedUrl = mb_strtolower("{$scheme}{$host}");
 
-        return ['name' => $normalizedUrl];
+        return new Url(['name' => $normalizedUrl]);
     }
 }
