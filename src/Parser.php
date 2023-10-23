002adcf75cf55c4e407c9ac118d3f48f77df9b7d
Konstantin Davydov <davydovks@mail.ru>
2023-10-23 10:32:14 +0500

    move string length limitation from reading to template

--- src/Parser.php
+++ src/Parser.php
@@ -16,9 +16,9 @@ class Parser
 
         $checkData = [
             'status_code' => $urlResponse->getStatusCode(),
-            'h1' => mb_substr($h1, 0, 255),
-            'title' => mb_substr($title, 0, 255),
-            'description' => mb_substr($description, 0, 255)
+            'h1' => $h1,
+            'title' => $title,
+            'description' => $description
         ];
 
         return $checkData;
