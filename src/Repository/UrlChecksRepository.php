89bd7ee3cf85484825571e692385cac7011ca39c
Konstantin Davydov <davydovks@mail.ru>
2023-11-13 16:45:21 +0500

    delete try-catch from repository classes

--- src/Repository/UrlChecksRepository.php
+++ src/Repository/UrlChecksRepository.php
@@ -11,59 +11,40 @@ class UrlChecksRepository
 
     public function __construct(Connection $pdo)
     {
-        try {
-            $this->pdo = $pdo->connect();
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-        }
+        $this->pdo = $pdo->connect();
     }
 
     public function all()
     {
         $sql = "SELECT * FROM url_checks ORDER BY id DESC";
-        try {
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->execute();
-            $checks = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-            return array_map(function ($check) {
-                return new UrlCheck($check);
-            }, $checks);
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-            return [];
-        }
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        $checks = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        return array_map(function ($check) {
+            return new UrlCheck($check);
+        }, $checks);
     }
 
     public function findAllByUrlId(int $urlId)
     {
         $sql = "SELECT * FROM url_checks WHERE url_id = ? ORDER BY id DESC";
-        try {
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->bindValue(1, $urlId, \PDO::PARAM_INT);
-            $stmt->execute();
-            $checks = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-            return array_map(function ($check) {
-                return new UrlCheck($check);
-            }, $checks);
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-            return [];
-        }
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(1, $urlId, \PDO::PARAM_INT);
+        $stmt->execute();
+        $checks = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        return array_map(function ($check) {
+            return new UrlCheck($check);
+        }, $checks);
     }
 
     public function findLastByUrlId(int $urlId)
     {
         $sql = "SELECT * FROM url_checks WHERE url_id = ? ORDER BY id DESC LIMIT 1";
-        try {
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->bindValue(1, $urlId, \PDO::PARAM_INT);
-            $stmt->execute();
-            $check = $stmt->fetch(\PDO::FETCH_ASSOC);
-            return is_array($check) ? new UrlCheck($check) : null;
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-            return null;
-        }
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(1, $urlId, \PDO::PARAM_INT);
+        $stmt->execute();
+        $check = $stmt->fetch(\PDO::FETCH_ASSOC);
+        return is_array($check) ? new UrlCheck($check) : null;
     }
 
     public function save(UrlCheck $urlCheck): void
@@ -71,17 +52,13 @@ class UrlChecksRepository
         $columns = "url_id, status_code, h1, title, description, created_at";
         $values = "?, ?, ?, ?, ?, ?";
         $sql = "INSERT INTO url_checks ({$columns}) VALUES ({$values}) RETURNING id";
-        try {
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->bindValue(1, $urlCheck->getUrlId());
-            $stmt->bindValue(2, $urlCheck->getStatusCode());
-            $stmt->bindValue(3, $urlCheck->getH1());
-            $stmt->bindValue(4, $urlCheck->getTitle());
-            $stmt->bindValue(5, $urlCheck->getDescription());
-            $stmt->bindValue(6, $urlCheck->getCreatedAt());
-            $stmt->execute();
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-        }
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(1, $urlCheck->getUrlId());
+        $stmt->bindValue(2, $urlCheck->getStatusCode());
+        $stmt->bindValue(3, $urlCheck->getH1());
+        $stmt->bindValue(4, $urlCheck->getTitle());
+        $stmt->bindValue(5, $urlCheck->getDescription());
+        $stmt->bindValue(6, $urlCheck->getCreatedAt());
+        $stmt->execute();
     }
 }
--- src/Repository/UrlsRepository.php
+++ src/Repository/UrlsRepository.php
@@ -11,72 +11,48 @@ class UrlsRepository
 
     public function __construct(Connection $pdo)
     {
-        try {
-            $this->pdo = $pdo->connect();
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-        }
+        $this->pdo = $pdo->connect();
     }
 
     public function all()
     {
         $sql = "SELECT * FROM urls ORDER BY id DESC";
-        try {
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->execute();
-            $urls = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-            return array_map(function ($url) {
-                return new Url($url);
-            }, $urls);
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-            return [];
-        }
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute();
+        $urls = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        return array_map(function ($url) {
+            return new Url($url);
+        }, $urls);
     }
 
     public function findOneById(int $id): ?Url
     {
         $sql = "SELECT * FROM urls WHERE id = ?";
-        try {
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->bindValue(1, $id);
-            $stmt->execute();
-            $url = $stmt->fetch(\PDO::FETCH_ASSOC);
-            return is_array($url) ? new Url($url) : null;
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-            return null;
-        }
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(1, $id);
+        $stmt->execute();
+        $url = $stmt->fetch(\PDO::FETCH_ASSOC);
+        return is_array($url) ? new Url($url) : null;
     }
 
     public function findOneByName(string $name): ?Url
     {
         $sql = "SELECT * FROM urls WHERE name = ?";
-        try {
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->bindValue(1, $name);
-            $stmt->execute();
-            $url = $stmt->fetch(\PDO::FETCH_ASSOC);
-            return is_array($url) ? new Url($url) : null;
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-            return null;
-        }
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(1, $name);
+        $stmt->execute();
+        $url = $stmt->fetch(\PDO::FETCH_ASSOC);
+        return is_array($url) ? new Url($url) : null;
     }
 
     public function save(Url $url): string
     {
         $sql = "INSERT INTO urls (name, created_at) VALUES (?, ?) RETURNING id";
-        try {
-            $stmt = $this->pdo->prepare($sql);
-            $stmt->bindValue(1, $url->getName());
-            $stmt->bindValue(2, $url->getCreatedAt());
-            $stmt->execute();
-            [$id] = $stmt->fetch(\PDO::FETCH_NUM);
-            return $id;
-        } catch (\PDOException $e) {
-            echo $e->getMessage();
-            return '';
-        }
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(1, $url->getName());
+        $stmt->bindValue(2, $url->getCreatedAt());
+        $stmt->execute();
+        [$id] = $stmt->fetch(\PDO::FETCH_NUM);
+        return $id;
     }
 }
